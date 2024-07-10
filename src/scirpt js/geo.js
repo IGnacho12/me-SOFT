0b63a6c05feac7481583fa41c63c2e88110e7b03
N4cho <castilloignacio1510@gmail.com>
2024-07-10 21:00:02 +0200

    test geolocation with javascript

---
+++ src/scirpt js/geo.js
@@ -0,0 +1,22 @@
+const options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  const total = [
+    console.log("Tu ubicación actual es:"),
+    console.log(`Latitud : ${crd.latitude}`),
+    console.log(`Longitud: ${crd.longitude}`),
+    console.log(`Más o menos ${crd.accuracy} metros.`),
+  ]
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
