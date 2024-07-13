9593f46d6d99dfc68aa6e95b94b083d3c00d799b
N4cho <castilloignacio1510@gmail.com>
2024-07-13 10:54:21 +0200

    update

--- src/scirpt js/geo.js
+++ src/scirpt js/geo.js
@@ -1,22 +1,22 @@
-const options = {
-  enableHighAccuracy: false,
-  timeout: 5000,
-  maximumAge: 0,
-};
+// const options = {
+//   enableHighAccuracy: false,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
 
-function success(pos) {
-  const crd = pos.coords;
+// function success(pos) {
+//   const crd = pos.coords;
 
-  const total = [
-    console.log("Tu ubicación actual es:"),
-    console.log(`Latitud : ${crd.latitude}`),
-    console.log(`Longitud: ${crd.longitude}`),
-    console.log(`Más o menos ${crd.accuracy} metros.`),
-  ]
-}
+//   const total = [
+//     console.log("Tu ubicación actual es:"),
+//     console.log(`Latitud : ${crd.latitude}`),
+//     console.log(`Longitud: ${crd.longitude}`),
+//     console.log(`Más o menos ${crd.accuracy} metros.`),
+//   ]
+// }
 
-function error(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
 
-navigator.geolocation.getCurrentPosition(success, error, options);
+// navigator.geolocation.getCurrentPosition(success, error, options);
